1c6ac4b4e63e02e9c07e1ef8a25dc82b6a7f4e6f
kimjaehak <kimjaehak512@gmail.com>
2024-12-19 15:45:22 +0900

    2241219 수정 및 코드 생성

--- 5_Trees/5_iterInorderVSthreadedInorder.c
+++ 5_Trees/5_iterInorderVSthreadedInorder.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <time.h>
+
+//Threaded부분은 이전 코드 그대로 가져왔음.
 
 typedef struct Node {
     char data;
@@ -17,7 +20,30 @@ void printInorder(Node* head);
 Node* insucc(Node* n);
 Node* inpre(Node* n);
 
+void iterInorder(Node* root);
 
+#define MAX_STACK 15
+typedef struct ElementforStack {
+    Node* node;
+    int phase;
+} Element;
+
+Element Stack[MAX_STACK];
+int top = -1;
+
+void push(Node* node, int phase) {
+    top++;
+    Stack[top].node = node;
+    Stack[top].phase = phase;
+}
+
+void pop() {
+    top--;
+}
+
+bool isEmpty() {
+    return top == -1 ? true : false;
+}
 
 int main() {
     Node* Tree_1 = newTree();   // 비어있는 새 트리 생성
@@ -34,10 +60,52 @@ int main() {
     tmp = insertRight(Tree_1->leftChild, newNode('B'));
     insertLeft(tmp, newNode('G'));
 
-    //inorder를 이용하여 트리 탐색결과 출력
-    printInorder(Tree_1);
+    clock_t start, end;
+
+    start = clock();
+    for (int i = 0; i < 500; i++) {
+        printInorder(Tree_1);
+    }
+    end = clock();
+    printf("스레드탐색 소요시간: %ld\n", end - start);
+
+    start = clock();
+    tmp = Tree_1->leftChild;
+    for (int i = 0; i < 500; i++) {
+        iterInorder(Tree_1);
+    }
+    end = clock();
+    printf("기본탐색 소요시간: %d", end - start);
+    //왜 이게 더 빠르냐;;;;;
+
 
     return 0;
+}
+
+void iterInorder(Node* root) {
+    top = -1;
+    for(push(root, 0); isEmpty() == false; ) {
+
+        if (Stack[top].node->leftThreaded != true && Stack[top].phase < 1) {
+            Stack[top].phase = 1;
+            push(Stack[top].node->leftChild, 0);
+            continue;
+        } // phase 1 end;
+
+        if (Stack[top].phase < 2) {
+            printf("%c ", Stack[top].node->data);
+            Stack[top].phase = 2;
+        } // phase 2 end;       // nomal code
+
+        if (Stack[top].node->rightThreaded != true && Stack[top].phase < 3) {
+            Stack[top].phase = 3;
+            push(Stack[top].node->rightChild, 0);
+            continue;
+        } // phase 2 end;
+
+        pop();
+        if (Stack[top].node->data == '\0') break;
+    }
 }
 
 Node* newNode(char data) {
