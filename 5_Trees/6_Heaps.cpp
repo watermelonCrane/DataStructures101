7bc9a1895a8736d3990324c485798d7946217a13
kimjaehak <kimjaehak512@gmail.com>
2025-02-10 15:27:28 +0900

    MAXHeap 구현 완료

--- 5_Trees/6_Heaps.cpp
+++ 5_Trees/6_Heaps.cpp
@@ -4,8 +4,6 @@
 #include <cmath>
 using namespace std;
 
-
-//일단 자료형 int로 만들고, templet과 Key를 넣어서 범용적이게 수정할 것.
 //인덱싱은 구현 안할 것임.
 //vector 기반이므로 full() method 불필요.
 class MaxHeap {
@@ -27,11 +25,36 @@ public:
     }
 
     int pop() {
+        if (empty()) throw runtime_error("Heap is empty!");     // 비어있으면 에러 발생
 
+        int result = tree[1];
+
+        tree[1] = tree.back();
+        tree.pop_back();
+
+        for (int i = 1; i * 2 <= tree.size() - 1; ) {   // 자식이 존재하는 동안 반복
+            if (tree[i] > tree[i * 2] && tree[i] > tree[i * 2 + 1]) break;
+            if (i * 2 == tree.size() - 1 || tree[i * 2] > tree[i * 2 + 1]) {    // LeftChild 만 존재하거나 LeftChild가 RightChild보다 클 경우
+                int tmp = tree[i * 2];
+                tree[i * 2] = tree[i];
+                tree[i] = tmp;
+                i *= 2;
+            }
+            else {  // Right Child가 더 크거나 같을 경우
+                int tmp = tree[i * 2 + 1];
+                tree[i * 2 + 1] = tree[i];
+                tree[i] = tmp;
+                i *= 2;
+                i++;
+            }
+        }
+
+        return result;
     }
 
     bool empty() {
-
+        if (tree.size() == 1) return true;
+        else return false;
     }
 
     void printHeap() {
@@ -49,17 +72,21 @@ public:
 
 int main() {
     MaxHeap h;
-    h.push(14);
-    h.push(15);
-    h.push(20);
+    
+    h.push(6);
     h.push(2);
-    h.push(10);
+    h.push(3);
+    h.push(1);
+    h.push(0);
 
     h.printHeap();
+    
 
-    h.push(21);
+    cout << '\n';
+    for (; !h.empty();) {
+        cout << h.pop() << ' ';
+    }
 
-    h.printHeap();
 
 
     return 0;
