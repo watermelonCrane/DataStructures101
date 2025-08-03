9b632f7fb44951364fbb3c70fd6f99532f5a9096
kimjaehak(Desktop) <kimjaehak512@gmail.com>
2025-08-04 06:22:19 +0900

    kmp 조금 더 올바른 방향으로 수정

--- 2_Arrays-and-Structures/1_KMP.c
+++ 2_Arrays-and-Structures/1_KMP.c
@@ -1,38 +1,46 @@
-//성공~!
-
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-// https://injae-kim.github.io/dev/2020/07/23/all-about-kmp-algorithm.html
+// 참고글 https://injae-kim.github.io/dev/2020/07/23/all-about-kmp-algorithm.html
 
-void makeFailure(char pat[], int failure[]) {
-    for (int i = 0, k = -1; i < strlen(pat); i++) {
-        if (i == 0 || pat[i] == pat[k]) {
-            failure[i] = k;
+int* failure(const char* pat) {
+    int patlen = strlen(pat);
+    int* pi = (int*)malloc(sizeof(int) * patlen);
+
+    int i, k;   // i: 선행자, k: 후행자
+    for (i = k = 0; i < patlen;) {
+        if (i == 0) {   // 처음 시작에서 pi[0]을 0으로 두고 시작함.
+            pi[i] = 0;
+            i++;
+        }
+
+        if (pat[i] == pat[k]) {     // 접두사와 접미사가 같다면 pi[i]의 배열에 몇글자가 일치하는지 기록
+            pi[i] = k + 1;
+            i++;
             k++;
         }
-        else if (k != 0) {      // 이 부분이 왜 필요했는지 생각해보자. "ababaa"패턴에서 이 조건문을 뺀다면?
-            k = 0;
-            i--;
+        else if (k == 0) {  // 접두사와 접미사가 끝까지 같지 않다면 pi[i] = 0 으로 설정
+            pi[i] = 0;
+            i++;
         }
-        else {
-            k = -1;
-            failure[i] = k;
-            k++;
+        else {  // 이어지는 접두사와 접미사가 일치하지 않을 때 바로 앞까지 같았던 정보를 참고하여 다시 비교 시작 (kmp와 같은 원리)
+            if (k == pi[k]) k--;
+            else k = pi[k];
         }
+
     }
 
+    return pi;
 }
 
-int kmp(char str[], char pat[]) {
-    int failure[20];
-    makeFailure(pat, failure);
-    
-    int lens = strlen(str);
-    int lenp = strlen(pat);
+int kmp(const char* str, const char* pat) {
+    int* pi = failure(pat);
+    int patlen = strlen(pat);
 
-    for (int i = 0, k = 0; i < lens && k < lenp; ) {
-        if (str[i] == pat [k]) {
+    int i, k;       // i: str배열 인덱스 포인팅, k: pat 배열 인덱스 포인팅
+    for (i = k = 0; k < patlen;) {
+        if (str[i] == pat[k]) {
             i++;
             k++;
         }
@@ -40,28 +48,31 @@ int kmp(char str[], char pat[]) {
             i++;
         }
         else {
-            k = failure[k - 1] + 1;
+            k = pi[k - 1];
         }
-
-        if (k == lenp) return i - lenp;
-
     }
 
-    return -1;
+    if (k >= patlen) {
+        return i - k;
+    } else return -1;
 
 }
 
 int main() {
+    const char* str = "aabcaabcabaabcaad";
+    const char* pat = "aabcaad";
 
-    freopen("1_testcase.txt", "r", stdin);
 
-    char str[2000];
-    char pat[20];
-    scanf("%s", str);
-    scanf("%s", pat);
-
+    // 패턴 위치 찾기
     int result = kmp(str, pat);
-    printf("%d", result);
+
+
+    //결과 출력
+    for (int i = result; i < result + strlen(pat); i++) {
+        printf("%c", str[i]);
+    }
+
+
 
     return 0;
 }
